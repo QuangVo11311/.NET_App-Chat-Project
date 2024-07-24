87e3387aac21f3c02abe0da2f9ee4546f878dfc1
Võ Đăng Quang <94774447+QuangVo11311@users.noreply.github.com>
2024-07-24 15:21:17 +0700

    Create MVC

---
+++ AppChat/AppChat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppChat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
